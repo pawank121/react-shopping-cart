439f1226d9a4ee3b08cc283b1a0468a02cf1566b
PAWAN KUMAR <121pawank@gmail.com>
2020-09-14 10:37:49 +0530

    cart component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import data from "./data.json";
 import Products from "./components/Products";
+import Filter from "./components/Filter";
+import Cart from "./components/Cart";
 
 class App extends React.Component {
     constructor() {
@@ -9,8 +11,73 @@ class App extends React.Component {
             products: data.products,
             size: "",
             sort: "",
+            cartItems: [],
         };
     }
+
+    filterProducts = (event) => {
+        console.log(event.target.value);
+        if (event.target.value === "") {
+            this.setState({
+                size: event.target.value,
+                products: data.products,
+            });
+        } else {
+            this.setState({
+                size: event.target.value,
+                products: data.products.filter((product) => product.availableSizes.indexOf(event.target.value) >= 0),
+            });
+        }
+    };
+
+    sortProducts = (event) => {
+        console.log(event.target.value);
+        const sort = event.target.value;
+        this.setState({
+            sort: sort,
+            products: this.state.products
+                .slice()
+                .sort((a, b) =>
+                    sort === "lowest"
+                        ? a.price > b.price
+                            ? 1
+                            : -1
+                        : sort === "highest"
+                        ? a.price < b.price
+                            ? 1
+                            : -1
+                        : a._id > b._id
+                        ? 1
+                        : -1
+                ),
+        });
+    };
+
+    removeFromCart = (product) => {
+        const cartItems = this.state.cartItems.slice();
+        this.setState({
+            cartItems: cartItems.filter((x) => x._id !== product._id),
+        });
+    };
+
+    addToCart = (product) => {
+        const cartItems = this.state.cartItems.slice();
+        let inCart = false;
+        cartItems.forEach((item) => {
+            if (item._id === product._id) {
+                item.count++;
+                inCart = true;
+            }
+        });
+
+        if (!inCart) {
+            cartItems.push({ ...product, count: 1 });
+        }
+
+        this.setState({
+            cartItems: cartItems,
+        });
+    };
 
     render() {
         return (
@@ -21,9 +88,18 @@ class App extends React.Component {
                 <main>
                     <div className="content">
                         <div className="main">
-                            <Products products={this.state.products}></Products>
+                            <Filter
+                                count={this.state.products.length}
+                                size={this.state.size}
+                                sort={this.state.sort}
+                                filterProducts={this.filterProducts}
+                                sortProducts={this.sortProducts}
+                            ></Filter>
+                            <Products products={this.state.products} addToCart={this.addToCart}></Products>
                         </div>
-                        <div className="sidebar">Cart Items</div>
+                        <div className="sidebar">
+                            <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart}></Cart>
+                        </div>
                     </div>
                 </main>
                 <footer>All rights is reserved.</footer>
